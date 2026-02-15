3591a669fdfcb439a9c3d37d542193b7f80a099d
Joe <shipton_joe@yahoo.co.uk>
2026-02-15 17:54:03 +0000

    Icon Adds

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -9,56 +9,61 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'The 30 Club',
-        short_name: '30 Club',
-        description: 'Track your plant diversity - eat 30 different plants per week',
-        theme_color: '#10b981',
-        background_color: '#ffffff',
-        display: 'standalone',
-        orientation: 'portrait',
-        start_url: '/',
-        icons: [
-          {
-            src: 'manifest-icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'manifest-icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'manifest-icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
+  registerType: 'autoUpdate',
+  includeAssets: [
+    'favicon.ico', 
+    'apple-touch-icon.png',
+  ],
+  manifest: {
+    name: 'The 30 Club',
+    short_name: '30 Club',
+    description: 'Track your plant diversity - eat 30 different plants per week',
+    theme_color: '#10b981',
+    background_color: '#ffffff',
+    display: 'standalone',
+    orientation: 'portrait',
+    start_url: '/',
+    icons: [
+      {
+        src: 'manifest-icon-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
       },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365,
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-        ],
+      {
+        src: 'manifest-icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
       },
-    }),
+      {
+        src: 'manifest-icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable', // For Android adaptive icons
+      },
+    ],
+  },
+  workbox: {
+    globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'google-fonts-cache',
+          expiration: {
+            maxEntries: 10,
+            maxAgeSeconds: 60 * 60 * 24 * 365,
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+}),
   ],
 
   build: {
